f373345e38c67a4788c0d4c60f94dca80b4a567a
lucas mac bookAir <>
2020-08-13 01:03:30 +0800

    no message

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { IndexViewComponent } from './views/indexView/IndexView.component';
+import { IndexViewComponent } from './views/indexView/indexView.component';
 import { ContactUsViewComponent } from './views/contactUsView/contactUsView.component';
 
 import { UserInfoComponent } from './components/userInfoComponent/userInfo.component'; //提醒:這個只是我在實驗用,他只是一般組件不是概念上為views的組件.
